99f575cf9ff3f8dad02770044661a8b3e4c79495
erislandy <erislandy.cabrales@gmail.com>
2018-11-04 18:02:32 -0800

    Initial Project

---
+++ Democracy/Models/State.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Democracy.Models
+{
+    public class State
+    {
+        [Key]
+        public int StateId { get; set; }
+
+        [Required(ErrorMessage = "The field {0} is required")]
+        [StringLength(50, ErrorMessage =
+                        "The field {0} can contain maximum {1} and minimun {2} character",
+                        MinimumLength = 3)]
+        [Display(Name = "State description")]
+        public string Description { get; set; }
+    }
+}
